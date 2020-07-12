5b93bd67c37389a533c95fdc313a5f1fafbddfcf
lenghanbing <1170204840@qq.com>
2020-07-12 22:03:20 +0800

    Update Customize.sh

--- All/Customize.sh
+++ All/Customize.sh
@@ -2,5 +2,5 @@
 #1.设置默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' lede/package/base-files/files/bin/config_generate
 #2.修改netgear主题
-cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
-cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
+#cp -f Theme/nlogo.png lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/nlogo.png
+#cp -f Theme/style.css lede/package/lean/luci-theme-netgear/htdocs/luci-static/netgear/css/style.css
